2c645dc295b42a6f41b3803f11d442bf4896a2ca
Jeremy Hurpy <jeremyhurpy@gmail.com>
2023-05-17 00:28:30 +0700

    finish and norme correct

--- src/creat_struct_map.c
+++ src/creat_struct_map.c
@@ -5,28 +5,37 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jhurpy <jhurpy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/11 23:30:37 by jhurpy            #+#    #+#             */
-/*   Updated: 2023/05/15 12:29:11 by jhurpy           ###   ########.fr       */
+/*   Created: 2023/05/16 22:56:09 by jhurpy            #+#    #+#             */
+/*   Updated: 2023/05/17 00:25:24 by jhurpy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
-#include "../libft/includes/libft.h"
 
 /*
-The function init_x_y_color initialize the x, y and color of each struct.
+The function creat_struct_map creat an array of struct to save the map.
 */
 
-static void	init_x_y_color(t_data1 **map)
+/*
+ERROR message to handle (malloc error)
+*/
+
+void	creat_struct_map(t_size size, t_map ***map)
 {
 	int		i;
 	int		j;
 
+	*map = (t_map **)malloc(sizeof(t_map *) * size.row);
+	if (*map == NULL)
+		exit (0);
 	i = 0;
 	j = 0;
-	while (map[i])
+	while (i < size.row)
 	{
-		while (map[i][j])
+		(*map)[i] = (t_map *)malloc(sizeof(t_map) * size.col);
+		if ((*map)[i] == NULL)
+			free_2d_array(*map, i);
+		while (j < size.col)
 		{
 			(*map)[i][j].x = i;
 			(*map)[i][j].y = j;
@@ -36,49 +45,4 @@ static void	init_x_y_color(t_data1 **map)
 		j = 0;
 		i++;
 	}
-}
-
-/*
-The function creat_struct_map creat a array of struct to save the map.
-*/
-
-t_data1	**creat_struct_map(t_data2 map_file) // creat a array of struct
-{
-	t_data1	**map;
-	int		i;
-	int		j;
-
-	i = 0;
-	map = (t_data1 **)malloc(sizeof(t_data1 *) * (map_file.rows + 1));
-	if (!map)
-		return (NULL); // ERROR message to handle (malloc error) do not forget to free
-	map[map_file.rows] = NULL;
-	while (i < map.rows)
-	{
-		map[i] = (t_data1 *)malloc(sizeof(t_data1) * (map_file.columns + 1));
-		if (!map[i])
-		{
-			free_2d_array(map);
-			return (NULL); // ERROR message to handle (malloc error) do not forget to free
-		}
-		map[i][map_file.columns] = NULL;
-		i++;
-	}
-	init_xy_struct(map);
-	return (map);
-}
-
-/*
-The function creat_map creat a array of struct to save the map.
-*/
-
-t_data1	**creat_map(char *file)
-{
-	t_data1	**map;
-	t_data2	map_file;
-
-	count_rows_columns(&map_file, file);
-	map = create_struct_map(map_file);
-
-	return (map);
 }
